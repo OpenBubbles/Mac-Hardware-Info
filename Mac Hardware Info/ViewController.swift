20cf07f54b021fa843a4f3304994668884b49ce3
Tae Hagen <tae.hagen@gmail.com>
2024-04-12 13:50:33 -0700

    Initial Commit

---
+++ Mac Hardware Info/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Mac Hardware Info
+//
+//  Created by User on 4/12/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
